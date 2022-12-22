07de4a7c820fa68e68ca00d1c999d941eae776ea
Ismail Chahir <ismailchahir01@gmail.com>
2022-12-22 18:48:23 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AppStyle.css";
+import Main from "./components/Main";
+
+export default function App() {
+  return (
+    <div className="container">
+      <Main />
+    </div>
+  );
+}
